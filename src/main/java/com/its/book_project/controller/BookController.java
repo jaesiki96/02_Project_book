c34d8fb3a500186353d381bbcd4fd95231aced57
jaesiki96 <jaesiki96@naver.com>
2022-12-27 09:25:06 +0900

    14_bookSave 기본세팅 완료

--- src/main/java/com/its/book_project/controller/BookController.java
+++ src/main/java/com/its/book_project/controller/BookController.java
@@ -1,10 +1,33 @@
 package com.its.book_project.controller;
 
+import com.its.book_project.dto.BookDTO;
+import com.its.book_project.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/book")
 public class BookController {
-    private final BookController bookController;
+    private final BookService bookService;
+
+    // 책 등록 페이지 출력
+    @GetMapping("/save")
+    public String saveForm() {
+        return "bookPages/bookSave";
+    }
+
+    // 책 등록 처리
+    @PostMapping("/save")
+    public String save(@ModelAttribute BookDTO bookDTO) throws IOException {
+        bookService.save(bookDTO);
+        return "bookPages/bookList";
+    }
+
 }
--- src/main/java/com/its/book_project/service/BookService.java
+++ src/main/java/com/its/book_project/service/BookService.java
@@ -1,11 +1,46 @@
 package com.its.book_project.service;
 
+import com.its.book_project.dto.BookDTO;
+import com.its.book_project.entity.BookEntity;
+import com.its.book_project.entity.BookFileEntity;
+import com.its.book_project.repository.BookFileRepository;
 import com.its.book_project.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
     private final BookRepository bookRepository;
+    private final BookFileRepository bookFileRepository;
+
+    // 책 등록 처리
+    public Long save(BookDTO bookDTO) throws IOException {
+        // 첨부파일 여부에 따라 로직 분리
+        if (bookDTO.getBookFile().isEmpty()) {
+            // 첨부파일 없음
+            BookEntity bookEntity = BookEntity.toSaveEntity(bookDTO);
+            return bookRepository.save(bookEntity).getId();
+        } else {
+            // 첨부파일 있음
+            MultipartFile bookFile = bookDTO.getBookFile();
+            String originalFileName = bookFile.getOriginalFilename();
+            String storedFileName = System.currentTimeMillis() + "_" + originalFileName;
+            String savePath = "D:\\springboot_img\\" + storedFileName;
+            bookFile.transferTo(new File(savePath));
+
+            BookEntity bookEntity = BookEntity.toSaveFileEntity(bookDTO);
+            Long savedId = bookRepository.save(bookEntity).getId();
+
+            BookEntity entity = bookRepository.findById(savedId).get();
+            BookFileEntity bookFileEntity =
+                    BookFileEntity.toSaveBookFileEntity(entity, originalFileName, storedFileName);
+            bookFileRepository.save(bookFileEntity);
+            return savedId;
+        }
+    }
 }
